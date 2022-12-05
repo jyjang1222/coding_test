2dbb1fe18d00c055835277c9a7d0a3e6efb45f7e
JJY <jyjang1222@naver.com>
2022-12-05 23:17:39 +0900

    first commit

---
+++ programmers/lv0/README.md
@@ -0,0 +1,20 @@
+# 프로그래머스 lv0 문제풀이
+## 배울사항
+### replace() 메서드
+```python
+str = 'hello world'
+str = str.replace('o', '', 1)
+print(str) # hell world
+```
+- replace(str, str, cnt)
+- 인자는 차례로 변경될 문자, 치환할 문자, 수행횟수 
+### in, not in 키워드
+```python
+str = 'hello world'
+bool = False
+if 'hell' in str:
+    bool = True
+print(bool) # True
+```
+- 데이터 안에 값이 있는지없는지 반환하는 키워드이다.
+- in 키워드는 안에 있으면 true반환 없으면 false반환 이고 not in은 그 반대이다.
---
+++ programmers/lv0/옹알이1.py
@@ -0,0 +1,73 @@
+'''
+머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다. 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다. 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ babbling의 길이 ≤ 100
+1 ≤ babbling[i]의 길이 ≤ 15
+babbling의 각 문자열에서 "aya", "ye", "woo", "ma"는 각각 최대 한 번씩만 등장합니다.
+즉, 각 문자열의 가능한 모든 부분 문자열 중에서 "aya", "ye", "woo", "ma"가 한 번씩만 등장합니다.
+문자열은 알파벳 소문자로만 이루어져 있습니다.
+
+예시
+["aya", "yee", "u", "maa", "wyeoo"],	1
+["ayaye", "uuuma", "ye", "yemawoo", "ayaa"],	3
+'''
+
+# 내풀이
+def solution(babbling):
+    arr = ['aya', 'ye', 'woo', 'ma']
+    arr2 = ['aya', 'ye', 'woo', 'ma']
+    cnt = 0
+    
+    # 조합2
+    for i in arr:
+        for j in arr:
+            if i != j:
+                tmp = i + j
+                arr2.append(tmp)
+                tmp = ''
+    # 조합3
+    for i in arr:
+        for j in arr:
+            for k in arr:
+                if i != j and i != k and j != k:
+                    tmp = i + j + k
+                    arr2.append(tmp)
+                    tmp = ''
+    # 조합4        
+    for i in arr:
+        for j in arr:
+            for k in arr:
+                for m in arr:
+                    if i != j and i != k and i != m and j != k and j != m and k != m:
+                        tmp = i + j + k + m
+                        arr2.append(tmp)
+                        tmp = ''
+                
+            
+    for i in arr2:
+        for j in babbling:
+            if i == j:
+                cnt += 1
+    answer = cnt
+    # print(arr2)
+    return answer
+
+test = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa", "ayayeaya", "woomayeaya"]
+# print(solution(test))
+
+# 정답
+def solution(babbling):
+    word = ['aya', 'ye', 'woo', 'ma']
+    count = 0
+    for i in babbling:
+        for j in word:
+            if j in i:
+                i = i.replace(j, '*', 1)
+        i = i.replace('*', '')
+        if i == '':
+            count += 1
+    answer = count
+    return answer
+
+print(solution(test))
